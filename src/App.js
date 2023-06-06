3713b222b94b21dc8891512a41559cb884c955d2
Dai Uminho <pedrofernandeslegsi@outlook.pt>
2023-06-06 15:55:46 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App () {
                   <Route path="perfil" element={<Perfil/>} />
         <Route path='pin' element={<Pin/>}/>
           <Route path='registo' element={<Registo/>}/>
-        <Route path="/vsc_frontend" element={<Recuperacao/>} />
+        <Route path="/recuperacao" element={<Recuperacao/>} />
           <Route path='alterar' element={<Alterar/>}/>
       </Routes>
     </div>
